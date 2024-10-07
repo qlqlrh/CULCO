af2fde08e1d1117c87501062f6d0dd3cca66964e
qlqlrh <dongin@pusan.ac.kr>
2024-10-07 15:49:10 +0900

    backend 프로젝트 생성

---
+++ backend/src/main/java/com/bsori/culco/CulcoApplication.java
@@ -0,0 +1,13 @@
+package com.bsori.culco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulcoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulcoApplication.class, args);
+	}
+
+}
